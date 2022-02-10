227970c2b7d4220ce76206a7d6db404e9a532abb
LorénzoTLCP <63541098+LorenzoS11@users.noreply.github.com>
2022-02-10 12:12:58 +0100

    uppg 2 klar

---
+++ .github/u01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uppgift-01</title>
+</head>
+<body>
+
+
+<?php 
+header("Content-type: text/html; charset=utf-8"); 
+if(empty($_GETUnamn1)){
+    echo '<h1>Valkommen!</h1>'; 
+} 
+else{ 
+$namn=filter_input(INPUT_GET, 'namn'); 
+echo "<h1>Valkommen ${namn}!</hl>"; 
+echo "<p>Namnet $namn innehaller ", strlen($namn), " tecken.<p>"; 
+} 
+?>
+
+
+
+</body>
+</html>
---
+++ .github/u02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+define("cars", [
+  "McLaren",
+  "BMW",
+  "Audi"
+  "Ford"
+  "Opel"
+]);
+
+
+echo[rand(0,6)];
+
+?> 
+
+</body>
+</html>
