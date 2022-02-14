49754b63c6751f5f73624ed2074003da24501fb9
LorénzoTLCP <63541098+LorenzoS11@users.noreply.github.com>
2022-02-14 11:20:10 +0100

    commit

--- .github/u03.php
+++ .github/u03.php
@@ -4,15 +4,15 @@
 
 <?php
 $t = date("H");
-echo "<p>The hour (of the server) is " . $t; 
+echo "<p>The hour (Jesus) is " . $t; 
 echo ", and will give the following message:</p>";
 
 if ($t < "10") {
-  echo "Have a good morning!";
+  echo "ha en bra dag!";
 } elseif ($t < "20") {
-  echo "Have a good day!";
+  echo "ha en bra kväll!";
 } else {
-  echo "Have a good night!";
+  echo "ha en dålig dag!";
 }
 ?>
  
